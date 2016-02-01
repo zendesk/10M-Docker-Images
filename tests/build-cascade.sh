e5f06348a566868a57542d2eb9249e3d41b04754
Ofir Petrushka <opetrushka@zendesk.com>
2016-02-01 16:42:35 +1100

    Restructure nginx images

--- tests/build-cascade.sh
+++ tests/build-cascade.sh
@@ -3,13 +3,17 @@
 # Setup
 ORIGINAL_DIR=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR/..
 
-# Future add here more tests
+cd $SCRIPT_DIR/..
 cd base/build/php/5.6/composer/
 make build-cascade
+
 cd $SCRIPT_DIR/..
 cd base/build/php/5.6/fpm/composer/
+make build-cascade
+
+cd $SCRIPT_DIR/..
+cd base/build/nginx/nginx_redirect_to_ssl
 make build-cascade
 
 # back to where we were
--- tests/clean-cascade.sh
+++ tests/clean-cascade.sh
@@ -3,10 +3,18 @@
 # Setup
 ORIGINAL_DIR=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR/..
 
-# Future add here more tests
+# composer
+cd $SCRIPT_DIR/..
 cd base/build/php/5.6/composer/
+make clean-cascade
+
+cd $SCRIPT_DIR/..
+cd base/build/php/5.6/fpm/composer/
+make clean-cascade
+
+cd $SCRIPT_DIR/..
+cd base/build/nginx/nginx_redirect_to_ssl
 make clean-cascade
 
 # back to where we were
