f6ef5c415d326a413a05a0ece8fe93d5fd9f5ecf
Ofir Petrushka <opetrushka@zendesk.com>
2016-02-01 16:33:37 +1100

    add php-fpm

--- tests/build-cascade.sh
+++ tests/build-cascade.sh
@@ -8,6 +8,9 @@ cd $SCRIPT_DIR/..
 # Future add here more tests
 cd base/build/php/5.6/composer/
 make build-cascade
+cd $SCRIPT_DIR/..
+cd base/build/php/5.6/fpm/composer/
+make build-cascade
 
 # back to where we were
 cd $ORIGINAL_DIR
--- tests/update-all.sh
+++ tests/update-all.sh
@@ -8,6 +8,9 @@ cd $SCRIPT_DIR/..
 # Future add here more tests
 cd base/build/php/5.6/
 make update 
+cd $SCRIPT_DIR/..
+cd base/build/php/5.6/fpm/
+make update
 
 # back to where we were
 cd $ORIGINAL_DIR
