6061e92c5b256aa2f3a459a631c250a79fc8fceb
Ofir Petrushka <opetrushka@zendesk.com>
2016-04-12 19:30:21 +1000

    cached dir for imported images

--- include/docker_images.mk
+++ include/docker_images.mk
@@ -1,31 +1,9 @@
-all:
-	echo 'Usage: see https://github.com/rounds/10M-Docker-Images/blob/master/base/README.md'
+SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-build:
-	if [ -a Dockerfile ]; then docker build --tag "$(IMAGE_NAME)" .; fi;
-
-build-parent:
-	$(MAKE) -C .. build-cascade
+include $(SELF_DIR)/docker_images_common.mk
 
 build-cascade: build-parent build
 
-clean:
-	if [ -a Dockerfile ]; then docker rmi -f "$(IMAGE_NAME)"; fi;
-
-clean-parent:
-	$(MAKE) -C .. clean-cascade
-
 clean-cascade: clean clean-parent
 
-push-only:
-	if [ -a Dockerfile ]; then docker push "$(IMAGE_NAME)"; fi;
-
-push: build push-only
-
-push-parent:
-	$(MAKE) -C .. push-cascade
-
 push-cascade: push-parent push
-
-run:
-	if [ -a Dockerfile ]; then docker run --rm -it "$(IMAGE_NAME)" bash; fi;
